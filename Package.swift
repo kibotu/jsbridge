3d75a78f4d04b10d7c2fdd21d5a7fdb12a67433e
Jan Rabe <cloudgazer3d@gmail.com>
2026-03-09 11:02:25 +0100

    wip swift package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "JSBridge",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "JSBridge", targets: ["JSBridge"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kibotu/Orchard", from: "1.0.8"),
+    ],
+    targets: [
+        .target(
+            name: "JSBridge",
+            dependencies: [
+                .product(name: "Orchard", package: "orchard"),
+            ],
+            path: "ios/JSBridge/Sources/JSBridge",
+            resources: [
+                .copy("Resources/bridge.js"),
+            ]
+        ),
+    ]
+)
